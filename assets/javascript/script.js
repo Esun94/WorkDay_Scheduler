1b93fa942124f8c17ea3f27dd974b6d12de46521
Eric Sun <ericmsun@gmail.com>
2021-08-13 18:01:20 -0400

    saving

---
+++ assets/javascript/script.js
@@ -0,0 +1,41 @@
+//Create our document read funstion to make sure nothing runs before we load the page
+$(document).ready(function() {
+
+    var saveBtnEl = $('.saveBtn')
+    // function to listen for click events on page
+    saveBtnEl.on('click', function saveEvent() {
+        console.log('saving!')
+        //grab the saved event options
+            // create variables to save user input and time
+    
+        //save to local storage
+    
+        //alert user that items have been saved to localStorage
+    
+        // create timeOut Value that removes save alert
+    
+    });
+    
+        
+    
+    
+    // Create time updater function()
+        //get current time
+    
+        // create our loop to go over all the (html)time blocks (compare ids)
+    
+        // if else statement to...
+        // check to see if we passed out time
+            // check out removeClass and addClass
+        // check to see if current hour matches (html time)physical hour
+    
+        // check to see if the time is in the future
+    
+    
+    // retrieve local storage and render items to correct rows
+    
+    
+    
+    
+    
+    })
